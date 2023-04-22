2cff27d594a269b374dffedcfc88f35484c12ca9
Pratheek Shetty <83195025+techshetty@users.noreply.github.com>
2023-04-22 14:00:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+tmr=document.getElementById('timer_main');
+	var pretimer = document.getElementById('preTimer');
+	var n;
+	function tStart(){n = parseInt(document.getElementById('cdown').value);pretimer.style.display="none";
+	tmr.style.display = "block";
+	uTime()}
+	function uTime() {if(n>=0){
+		setTimeout(function(){tmr.innerHTML=n;n=n-1;uTime()},1000)
+	}
+	}
